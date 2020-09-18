30dbebc3e4cb3608b861db10a9e9bf9d7f86ac7e
Ryan Tan <ryan-wende@outlook.com>
2020-09-18 14:51:26 +0800

    Initial Commit

---
+++ Assets/Scripts/DebugConsole.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Rendering;
+
+public class DebugConsole : MonoBehaviour
+{
+    [Header("Console Properties")]
+    public bool showConsole = false;
+    public bool showCommandHelp = false;
+    private string inputString;
+    private Vector2 commandListScroll;
+
+    [Header("Console Commands")]
+    public static DebugCommand setStone;
+    public static DebugCommand setWood;
+    public static DebugCommand<int> setGold;
+    public static DebugCommand quitGame;
+    public static DebugCommand help;
+
+    public List<object> commandList;
+
+    private void Awake()
+    {
+        quitGame = new DebugCommand("Quit_Game", "Quits the game entirely.", "Quit_Game", () =>
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        });
+
+        setWood = new DebugCommand("Set_Wood", "Sets your current stone amount to 1000.", "Set_Wood", () =>
+        {
+            DummyGameManager.dummyGameManager._currentWood += 1000;
+        });
+
+        setGold = new DebugCommand<int>("Set_Gold", "Sets your current gold amount to the given number.", "Set_Gold <Gold_Amount>", (x) =>
+        {
+            DummyGameManager.dummyGameManager._currentGold += x;
+        });
+
+        help = new DebugCommand("Help", "Shows a list of commands.", "Help", () =>
+        {
+            showCommandHelp = true;
+        });
+
+        commandList = new List<object>
+        {
+            quitGame,
+            setWood,
+            setGold,
+            help
+        };
+    }
+
+    public void OnToggleDebug(bool value)
+    {
+        showConsole = value;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            showConsole = !showConsole;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (showConsole)
+            {
+                HandleInput();
+                inputString = "";
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (!showConsole)
+        {
+            return;
+        }
+
+        float y = 0f;
+
+        if (showCommandHelp)
+        {
+            GUI.Box(new Rect(0, y, Screen.width, 100), "");
+            Rect viewport = new Rect(0, 0, Screen.width - 30, 20 * commandList.Count);
+            commandListScroll = GUI.BeginScrollView(new Rect(0, y + 5f, Screen.width, 90), commandListScroll, viewport);
+
+            for (int i = 0; i < commandList.Count; i++)
+            {
+                DebugCommandBase command = commandList[i] as DebugCommandBase;
+                string label = $"{command._commandFormat} = {command._commandDescription}";
+                Rect labelRect = new Rect(5, 20 * i, viewport.width - 100, 20);
+                GUI.Label(labelRect, label);
+            }
+            GUI.EndScrollView();      
+            y += 100;
+        }
+
+        GUI.Box(new Rect(0, y, Screen.width, 30), "");
+        GUI.Label(new Rect(0, y+ 30, Screen.width, 30), "Type 'Help' for all commands.");
+        GUI.backgroundColor = new Color(0, 0, 0, 0);
+        inputString = GUI.TextField(new Rect(10f, y + 5f, Screen.width - 20f, 20f), inputString);
+    }
+
+    private void HandleInput()
+    {
+        string[] properties = inputString.Split(' ');
+        for (int i = 0; i < commandList.Count; i++)
+        {
+            DebugCommandBase commandBase = commandList[i] as DebugCommandBase;
+            
+            if (inputString.Contains(commandBase._commandID))
+            {
+                if (commandList[i] as DebugCommand != null)
+                {
+                    (commandList[i] as DebugCommand).Invoke();
+                }
+
+                else if (commandList[i] as DebugCommand<int> != null)
+                {
+                    (commandList[i] as DebugCommand<int>).Invoke(int.Parse(properties[1]));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DummyGameManager.cs
@@ -0,0 +1,58 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DummyGameManager : MonoBehaviour
+{
+    [Header("Resource Properties")]
+    private int currentGold;
+    private int currentStone;
+    private int currentWood;
+
+    public int _currentGold
+    {
+        get { return this.currentGold; }
+        set 
+        {
+            currentGold = value;
+            goldUITextField.text = "Current Gold: " + currentGold.ToString();
+        }
+    }
+    public int _currentStone
+    {
+        get { return this.currentStone; }
+        set
+        {
+            currentStone = value;
+            stoneUITextField.text = "Current Stone: " + currentStone.ToString();
+        }
+    }
+
+    public int _currentWood
+    {
+        get { return this.currentWood; }
+        set
+        {
+            currentWood = value;
+            woodUITextField.text = "Current Wood: " + currentWood.ToString();
+        }
+    }
+
+    [Header("UI Properties")]
+    [SerializeField] private Text goldUITextField;
+    [SerializeField] private Text stoneUITextField;
+    [SerializeField] private Text woodUITextField;
+
+    public static DummyGameManager dummyGameManager;
+
+    private void Awake()
+    {
+        dummyGameManager = this;
+        _currentGold = 0;
+        _currentStone = 0;
+        _currentWood = 0;
+    }
+
+}
